3127aab5b13a176961b62e2eaa70f43a35a5874a
Anikey Vladislav <dev.anikey.va@mail.ru>
2023-06-22 21:47:09 +0300

    clear template in project

---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="title">Hello KinoList!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Hello'
+}
+</script>
+
+<style scoped lang="sass">
+
+.title
+  text-align: center
+
+</style>
+

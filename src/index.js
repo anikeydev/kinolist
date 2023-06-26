b9ba59d0f3149dde453439b01cd503083a1ce03c
anikeydev <dev.anikey.va@mail.ru>
2023-06-26 23:37:33 +0300

    created function request films to genre from kinopoisk and view films list

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import './styles/style.sass'
-import apiRequestGenres from './api'
+import {apiRequestFilmToGenre, apiRequestGenres} from './api'
 
 const genresArr = await apiRequestGenres()
-const genresList = genresArr.map(item => {
-    return item.genre
-}).slice(0, 15)
+const topGenres = genresArr.slice(0, 10)
+// console.log(topGenres);
 
 
-const genresListHtml = genresList.map(item => {
-    return `<li class="categories__item">${item}</li>`
+const topGenresListHtml = topGenres.map(item => {
+    return `<li class="categories__item" data-id="${item.id}">${item.genre}</li>`
 })
 
 const $categories = document.querySelector('.main__categories')
 
-for (const genreHtml of genresListHtml) {
+for (const genreHtml of topGenresListHtml) {
     $categories.insertAdjacentHTML('beforeend', genreHtml)
+}
+
+const filmsToGenreArr =  await apiRequestFilmToGenre()
+const topFilmsToGenre = filmsToGenreArr.items.map(item => item).slice(0, 10)
+// console.log(topFilmsToGenre)
+const topFilmsToGenreListHtml = topFilmsToGenre.map(item => {
+    return `
+        <li class="filmsList__film-item film-item" data-kid="${item.kinopoiskId}">
+            <img src="${item.posterUrl}" alt="${item.nameOriginal}"/>
+            <p class="film-item__name">${item.nameRu}</p>
+        </li>
+    `
+})
+
+const $filmsList = document.querySelector('.filmsList')
+
+for (const filmHtml of topFilmsToGenreListHtml) {
+    $filmsList.insertAdjacentHTML('beforeend', filmHtml)
 }
